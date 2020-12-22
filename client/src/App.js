cf1060efa9037b1969119baac5c2eb0ae4058c68
Paul Jaramillo <paulejaramillo@gmail.com>
2020-12-22 15:17:28 -0500

    Create pages folder Create Home page Render Home page in App using Router and Route from react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Route path="/" exact render={() => <Home />} />
+    </Router>
+  );
 }
 
 export default App;
