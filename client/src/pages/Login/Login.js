339f93f68e53cf951768135783cc244dace05a55
Paul Jaramillo <paulejaramillo@gmail.com>
2020-12-23 12:56:54 -0500

    Add login form and axios client side post logic

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Login.css";
+import Axios from "axios";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginHandler = () => {
+    Axios.post("http://localhost:3001/user/login", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+  return (
+    <div className="Login">
+      <div className="LoginForm">
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="username..."
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="password..."
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button onClick={loginHandler}>Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
