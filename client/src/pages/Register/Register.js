a03f1168bc0d98da020ee15f1c214bbc8cff5acd
Paul Jaramillo <paulejaramillo@gmail.com>
2020-12-23 10:06:46 -0500

    Add client side logic to Register Form
    Use axios to make Register Form post call

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.css";
 
+import Axios from "axios";
+
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitRegister = () => {
+    Axios.post("http://localhost:3001/user/register", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div className="Register">
       <div className="RegisterForm">
         <h2>Register</h2>
-        <input type="text" placeholder="username..." />
-        <input type="text" placeholder="password..." />
-        <button>Register</button>
+        <input
+          type="text"
+          placeholder="username..."
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="password..."
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button onClick={submitRegister}>Register</button>
       </div>
     </div>
   );
