52b6b5eb3a9e0a315f957a2bd9bbd87fcb1b0487
Paul Jaramillo <paulejaramillo@gmail.com>
2020-12-23 14:48:15 -0500

    Add useEffect check for loggedIn in Home.js

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Home = () => {
+  // check in localstorage if loggedIn
+  useEffect(() => {
+    if (!localStorage.getItem("loggedIn")) {
+      localStorage.setItem("loggedIn", false);
+    }
+  }, []);
   return <div>Home</div>;
 };
 
